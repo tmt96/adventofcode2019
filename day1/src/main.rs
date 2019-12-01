f5cedcef0c686eb49568df3f0ef183a91444abc1
Tuan Tran <tuanmt@fb.com>
2019-12-01 11:02:59 -0800

    day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::cmp::max;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+fn part1(module: &Vec<i32>) -> i32 {
+    module.iter().map(|i| max(i / 3 - 2, 0)).sum()
+}
+
+fn part2(module: &Vec<i32>) -> i32 {
+    let transform_fn = |i: &i32| -> i32 {
+        let (mut i, mut result) = (*i, 0);
+        while i > 0 {
+            i = max(i / 3 - 2, 0);
+            result += i;
+        }
+        result
+    };
+
+    module.iter().map(transform_fn).sum()
+}
+
+fn main() -> std::io::Result<()> {
+    let filepath = Path::new("./input/day1.txt");
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    let modules: Vec<_> = reader
+        .lines()
+        .filter_map(Result::ok)
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect();
+
+    println!("part 1: {}", part1(&modules));
+    println!("part 2: {}", part2(&modules));
+    Ok(())
 }
