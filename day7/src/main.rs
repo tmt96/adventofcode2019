800d95e0fdc60adc13b0d3a7d2fbf73662331f02
tmt <minhtuan.tran96@gmail.com>
2019-12-20 00:17:27 -0800

    day7 pt2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -4,13 +4,18 @@ use std::path::Path;
 
 use itertools::Itertools;
 
+enum ResultCode {
+    Output(i64),
+    Terminated,
+}
+
 enum Mode {
     Position,
     Intermediate,
 }
 
 impl Mode {
-    fn from_i32(val: i32) -> Self {
+    fn from_i64(val: i64) -> Self {
         match val {
             0 => Self::Position,
             1 => Self::Intermediate,
@@ -20,13 +25,13 @@ impl Mode {
 }
 
 struct IntCodeComputer {
-    program: Vec<i32>,
-    input: VecDeque<i32>,
+    program: Vec<i64>,
+    input: VecDeque<i64>,
     inst_pointer: usize,
 }
 
 impl IntCodeComputer {
-    fn new(program: &[i32], phase: i32) -> Self {
+    fn new(program: &[i64], phase: i64) -> Self {
         Self {
             program: program.to_vec(),
             input: VecDeque::from(vec![phase]),
@@ -34,17 +39,17 @@ impl IntCodeComputer {
         }
     }
 
-    fn parse_instruction(&self) -> (i32, Mode, Mode) {
+    fn parse_instruction(&self) -> (i64, Mode, Mode) {
         let inst = self.program[self.inst_pointer];
         let opcode = inst % 100;
         let mut inst = inst / 100;
         let mode_1 = inst % 10;
         inst /= 10;
         let mode_2 = inst % 10;
-        (opcode, Mode::from_i32(mode_1), Mode::from_i32(mode_2))
+        (opcode, Mode::from_i64(mode_1), Mode::from_i64(mode_2))
     }
 
-    fn get_val(&self, loc: usize, mode: Mode) -> i32 {
+    fn get_val(&self, loc: usize, mode: Mode) -> i64 {
         let res = self.program[loc];
         match mode {
             Mode::Intermediate => res,
@@ -52,9 +57,8 @@ impl IntCodeComputer {
         }
     }
 
-    fn run_program(&mut self, input: i32) -> i32 {
+    fn run_one_turn(&mut self, input: i64) -> ResultCode {
         self.input.push_back(input);
-
         while self.inst_pointer < self.program.len() {
             let (opcode, mode_1, mode_2) = self.parse_instruction();
             match opcode {
@@ -82,9 +86,9 @@ impl IntCodeComputer {
                     self.inst_pointer += 2;
                 }
                 4 => {
-                    self.input
-                        .push_back(self.get_val(self.inst_pointer + 1, mode_1));
+                    let output = self.get_val(self.inst_pointer + 1, mode_1);
                     self.inst_pointer += 2;
+                    return ResultCode::Output(output);
                 }
                 5 => {
                     let (fst, snd) = (
@@ -126,44 +130,128 @@ impl IntCodeComputer {
                     self.program[dst as usize] = if fst == snd { 1 } else { 0 };
                     self.inst_pointer += 4;
                 }
-                99 => break,
+                99 => return ResultCode::Terminated,
                 opcode => panic!(
                     "Opcode must be 1, 2, 3, 4, 5, 6, 7, 8 or 99, receive {} at {}",
                     opcode, self.inst_pointer,
                 ),
             }
         }
+        unreachable!()
+    }
+
+    fn run_program(&mut self, input: i64) -> i64 {
+        let mut input = input;
+        while let ResultCode::Output(i) = self.run_one_turn(input) {
+            input = i;
+        }
         self.input[0]
     }
 }
 
-fn read_input(filepath: &Path) -> std::io::Result<Vec<i32>> {
+fn cal_normal_thrust(phases: &[i64], program: &[i64]) -> i64 {
+    phases.iter().fold(0, |state, &phase| {
+        IntCodeComputer::new(program, phase).run_program(state)
+    })
+}
+
+fn cal_thurst_with_feedback(phases: &[i64], program: &[i64]) -> i64 {
+    let mut computers: Vec<_> = phases
+        .iter()
+        .map(|&phase| IntCodeComputer::new(program, phase))
+        .collect();
+    let mut input = 0;
+    for i in (0..computers.len()).cycle() {
+        match computers[i].run_one_turn(input) {
+            ResultCode::Output(i) => input = i,
+            ResultCode::Terminated => return input,
+        }
+    }
+    unreachable!()
+}
+
+fn read_input(filepath: &Path) -> std::io::Result<Vec<i64>> {
     Ok(read_to_string(filepath)?
         .split(',')
-        .filter_map(|s| s.trim().parse::<i32>().ok())
+        .filter_map(|s| s.trim().parse::<i64>().ok())
         .collect())
 }
 
-fn part1(input: &[i32]) -> i32 {
+fn part1(input: &[i64]) -> i64 {
     (0..5)
         .permutations(5)
-        .map(|perm| {
-            perm.iter().fold(0, |state, &x| {
-                IntCodeComputer::new(input, x).run_program(state)
-            })
-        })
+        .map(|perm| cal_normal_thrust(&perm, input))
         .max()
         .unwrap()
 }
 
-fn part2(input: &[i32]) -> i32 {
-    IntCodeComputer::new(input, 1).run_program(0)
+fn part2(input: &[i64]) -> i64 {
+    (5..10)
+        .permutations(5)
+        .map(|perm| cal_thurst_with_feedback(&perm, input))
+        .max()
+        .unwrap()
 }
 
 fn main() -> std::io::Result<()> {
     let filepath = Path::new("./input/input.txt");
     let input = read_input(filepath)?;
     println!("part 1: {}", part1(&input));
-    // println!("part 2: {}", part2(&input));
+    println!("part 2: {}", part2(&input));
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day7_test1() {
+        let code = vec![
+            3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0,
+        ];
+        let res = part1(&code);
+        assert_eq!(43210, res);
+    }
+
+    #[test]
+    fn day7_test2() {
+        let code = vec![
+            3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23,
+            99, 0, 0,
+        ];
+        let res = part1(&code);
+        assert_eq!(54321, res);
+    }
+
+    #[test]
+    fn day7_test3() {
+        let code = vec![
+            3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1,
+            33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0,
+        ];
+        let res = part1(&code);
+        assert_eq!(65210, res);
+    }
+
+    #[test]
+    fn day7_test4() {
+        let code = vec![
+            3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1,
+            28, 1005, 28, 6, 99, 0, 0, 5,
+        ];
+        let res = part2(&code);
+        assert_eq!(139_629_729, res);
+    }
+
+    #[test]
+    fn day7_test5() {
+        let code = vec![
+            3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+            -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+            53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10,
+        ];
+        let res = part2(&code);
+        assert_eq!(18216, res);
+    }
 }
