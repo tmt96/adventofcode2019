d8ca42e8dfed7af39d2eb3c288817a8910247097
tmt <minhtuan.tran96@gmail.com>
2020-01-04 14:57:56 -0800

    day 12 pt 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -69,6 +69,18 @@ impl Planet {
         }
     }
 
+    fn eq_axis_x(&self, other: &Self) -> bool {
+        self.location.x == other.location.x && self.velocity.x == other.velocity.x
+    }
+
+    fn eq_axis_y(&self, other: &Self) -> bool {
+        self.location.y == other.location.y && self.velocity.y == other.velocity.y
+    }
+
+    fn eq_axis_z(&self, other: &Self) -> bool {
+        self.location.z == other.location.z && self.velocity.z == other.velocity.z
+    }
+
     fn potential_energy(&self) -> i64 {
         self.location.manhattan_len()
     }
@@ -103,8 +115,59 @@ fn simulate(planets: &[Planet], steps: i64) -> i64 {
         .sum()
 }
 
+fn gcd(a: i64, b: i64) -> i64 {
+    match (a, b) {
+        (a, b) if b > a => gcd(b, a),
+        (a, 0) => a,
+        (a, b) => gcd(a % b, b),
+    }
+}
+
+fn lcm(a: i64, b: i64) -> i64 {
+    a / gcd(a, b) * b
+}
+
 fn part1(planets: &[Planet]) -> i64 {
     simulate(planets, 1000)
+}
+
+fn part2(planets: &[Planet]) -> i64 {
+    let (mut x, mut y, mut z) = (None, None, None);
+    let mut cur_planets = planets.to_vec();
+    let mut counter = 0;
+    while x.is_none() || y.is_none() || z.is_none() {
+        counter += 1;
+        cur_planets = take_one_turn(&cur_planets);
+
+        if cur_planets
+            .iter()
+            .zip(planets)
+            .all(|(new_planet, og_planet)| og_planet.eq_axis_x(new_planet))
+            && x.is_none()
+        {
+            x = Some(counter)
+        }
+
+        if cur_planets
+            .iter()
+            .zip(planets)
+            .all(|(new_planet, og_planet)| og_planet.eq_axis_y(new_planet))
+            && y.is_none()
+        {
+            y = Some(counter)
+        }
+
+        if cur_planets
+            .iter()
+            .zip(planets)
+            .all(|(new_planet, og_planet)| og_planet.eq_axis_z(new_planet))
+            && z.is_none()
+        {
+            z = Some(counter)
+        }
+    }
+
+    lcm(lcm(x.unwrap(), y.unwrap()), z.unwrap())
 }
 
 fn main() {
@@ -115,13 +178,25 @@ fn main() {
         Planet::new(-8, 0, 4),
     ];
     println!("part 1: {}", part1(&planets));
+    println!("part 2: {}", part2(&planets));
 }
 
 mod tests {
     use super::*;
 
     #[test]
-    fn test1() {
+    fn test_pt1_1() {
+        let planets = vec![
+            Planet::new(-1, 0, 2),
+            Planet::new(2, -10, -7),
+            Planet::new(4, -8, 8),
+            Planet::new(3, 5, -1),
+        ];
+        assert_eq!(simulate(&planets, 10), 179)
+    }
+
+    #[test]
+    fn test_pt1_2() {
         let planets = vec![
             Planet::new(-8, -10, 0),
             Planet::new(5, 5, 10),
@@ -132,13 +207,24 @@ mod tests {
     }
 
     #[test]
-    fn test2() {
+    fn test_pt2_1() {
         let planets = vec![
             Planet::new(-1, 0, 2),
             Planet::new(2, -10, -7),
             Planet::new(4, -8, 8),
             Planet::new(3, 5, -1),
         ];
-        assert_eq!(simulate(&planets, 10), 179)
+        assert_eq!(part2(&planets), 2772)
+    }
+
+    #[test]
+    fn test_pt2_2() {
+        let planets = vec![
+            Planet::new(-8, -10, 0),
+            Planet::new(5, 5, 10),
+            Planet::new(2, -7, 3),
+            Planet::new(9, -8, -3),
+        ];
+        assert_eq!(part2(&planets), 4_686_774_924)
     }
 }
