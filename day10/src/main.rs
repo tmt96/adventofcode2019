224551613dfde095b2ec17192f598ab85550ef6a
tmt <minhtuan.tran96@gmail.com>
2020-01-04 22:10:15 -0800

    day 10 pt 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -3,10 +3,16 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
+use itertools::Itertools;
+
 type Position = (i32, i32);
 
-fn difference((row1, col1): Position, (row2, col2): Position) -> Position {
-    (row1 - row2, col1 - col2)
+fn difference((col1, row1): Position, (col2, row2): Position) -> Position {
+    (col1 - col2, row1 - row2)
+}
+
+fn manhattan_len((col, row): &Position) -> i32 {
+    col.abs() + row.abs()
 }
 
 fn gcd(a: i32, b: i32) -> i32 {
@@ -19,9 +25,9 @@ fn gcd(a: i32, b: i32) -> i32 {
     }
 }
 
-fn minimize_ratio((row, col): Position) -> Position {
-    let gcd = gcd(row, col);
-    (row / gcd, col / gcd)
+fn minimize_ratio((col, row): Position) -> Position {
+    let gcd = gcd(col, row);
+    (col / gcd, row / gcd)
 }
 
 fn read_input(filepath: &Path) -> std::io::Result<Vec<Position>> {
@@ -37,7 +43,7 @@ fn read_input(filepath: &Path) -> std::io::Result<Vec<Position>> {
                 .enumerate()
                 .filter_map(move |(col, ch)| {
                     if ch == '#' {
-                        Some((row as i32, col as i32))
+                        Some((col as i32, row as i32))
                     } else {
                         None
                     }
@@ -47,27 +53,66 @@ fn read_input(filepath: &Path) -> std::io::Result<Vec<Position>> {
         .collect())
 }
 
-fn part1(input: &[Position]) -> i32 {
+fn gun_loc(input: &[Position]) -> (Position, i32) {
     input
         .iter()
         .map(|&point| {
-            input
+            let asteroid_detected_count = input
                 .iter()
                 .filter(|&&snd_point| snd_point != point)
                 .map(|&snd_point| minimize_ratio(difference(point, snd_point)))
                 .collect::<HashSet<_>>()
-                .len()
+                .len();
+            (point, asteroid_detected_count as i32)
         })
-        .max()
-        .unwrap() as i32
+        .max_by_key(|(_, count)| *count)
+        .unwrap()
+}
+
+fn part1(input: &[Position]) -> i32 {
+    gun_loc(input).1
 }
 
 fn part2(input: &[Position]) -> i32 {
-    0
+    let loc = gun_loc(input).0;
+    println!("loc: col: {}, row: {}", loc.0, loc.1);
+    let mut positions: Vec<_> = input
+        .to_vec()
+        .into_iter()
+        .filter(|&snd_point| snd_point != loc)
+        .collect();
+    positions.sort_by_key(|&pos| manhattan_len(&difference(pos, loc)));
+
+    let map = positions
+        .iter()
+        .map(|&point| (minimize_ratio(difference(point, loc)), point))
+        .into_group_map();
+    let mut angle_points_list: Vec<_> = map
+        .into_iter()
+        .map(|((col, row), point)| ((col as f64).atan2(row as f64), point))
+        .collect();
+    angle_points_list.sort_by(|(angle1, _), (angle2, _)| angle2.partial_cmp(angle1).unwrap());
+
+    let mut planet_iters: Vec<_> = angle_points_list
+        .into_iter()
+        .map(|(_, points)| points.into_iter())
+        .collect();
+
+    let mut counter = 0;
+    for i in (0..planet_iters.len()).cycle() {
+        if let Some((col, row)) = planet_iters[i].next() {
+            println!("col: {}, row: {}", col, row);
+            counter += 1;
+            if counter == 200 {
+                return col * 100 + row;
+            }
+        }
+    }
+    unreachable!()
 }
 
 fn main() -> std::io::Result<()> {
-    let filepath = Path::new("./input/sample.txt");
+    let filepath = Path::new("./input/input.txt");
     let input = read_input(filepath)?;
     println!("part 1: {}", part1(&input));
     println!("part 2: {}", part2(&input));
